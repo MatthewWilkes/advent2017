a4241df6b192ddeca2af50fa1ffa74697b4f2d46
Matthew Wilkes <git@matthewwilkes.name>
2017-12-03 13:23:39 +0000

    Refactor coordinate variables to a struct

--- three/src/main.rs
+++ three/src/main.rs
@@ -1,11 +1,18 @@
 use std::io;
 
+struct Coordinate {
+    pub x: i32,
+    pub y: i32,
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read input");
     let target_number: u32 = input.trim().parse().expect("Did not get an integer");
-    let mut x: i32 = 0;
-    let mut y: i32 = 0;
+    let mut coord = Coordinate{
+        x: 0,
+        y: 0
+    };
     // The pattern of movements is right, up, left, down repeated.
     let mut part = 0;
     // The distances are 1, 1, 2, 2, 3, 3, 4, 4 etc
@@ -14,10 +21,10 @@ fn main() {
         step += 1;
         // The part remainder 4 is the direction we are going
         match part % 4 {
-            0 => x += 1,
-            1 => y += 1,
-            2 => x -= 1,
-            3 => y -= 1,
+            0 => coord.x += 1,
+            1 => coord.y += 1,
+            2 => coord.x -= 1,
+            3 => coord.y -= 1,
             _ => println!("Huh?"),
         }
         // The number of steps to finish a part starts at 1 and goes up every 2 parts
@@ -26,7 +33,7 @@ fn main() {
             part += 1;
         }
     }
-    println!("Memory address {} is at {},{}", target_number,x,y);
-    let distance = x.abs() + y.abs();
+    println!("Memory address {} is at {},{}", target_number, coord.x, coord.y);
+    let distance = coord.x.abs() + coord.y.abs();
     println!("Manhattan distance is {}", distance);
 }
