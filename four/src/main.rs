921f2d7d4ee89cec865b92fdffae90da9f79d87b
Matthew Wilkes <git@matthewwilkes.name>
2017-12-04 19:53:25 +0000

    Add passphrase validator

---
+++ four/src/main.rs
@@ -0,0 +1,22 @@
+use std::io::{self, Read};
+
+fn validate_password(passphrase: &str) -> bool {
+    let mut words: Vec<&str> = Vec::new();
+    for word in passphrase.split_whitespace() {
+        if words.contains(&word) {
+            return false;
+        } else {
+            words.push(&word);
+        }
+    }
+    return true;
+}
+
+fn main() {
+    let mut passwords = String::new();
+    let stdin = io::stdin();
+    let mut stdin_open = stdin.lock();
+    stdin_open.read_to_string(&mut passwords).expect("Couldn't read passwords");
+    let valid_passwords: Vec<&str> = passwords.lines().filter(|word| validate_password(word)).collect();
+    println!("Number valid is {}", valid_passwords.len());
+}
