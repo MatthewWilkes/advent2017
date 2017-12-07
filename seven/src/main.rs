b610c252255755d3c640e9d2fd98c1c5ca9170d5
Matthew Wilkes <git@matthewwilkes.name>
2017-12-07 20:31:32 +0000

    holy fuck this one is mad

--- seven/src/main.rs
+++ seven/src/main.rs
@@ -6,6 +6,14 @@ use indextree::{Arena, NodeId};
 extern crate regex;
 extern crate indextree;
 
+fn weight(node: NodeId, tree: &Arena<&str>, weights: &HashMap<&str, i32>) -> i32 {
+    let mut weight = 0;
+    for child in node.descendants(tree) {
+        weight += weights.get(tree[child].data).unwrap();
+    }
+    return weight;
+}
+
 fn main() {
     let mut edges_string = String::new();
     let stdin = io::stdin();
@@ -15,10 +23,10 @@ fn main() {
 
     let tree = &mut Arena::new();
     let mut nodes: HashMap<&str, NodeId> = HashMap::new();
+    let mut weights: HashMap<&str, i32> = HashMap::new();
 
     let edges = Regex::new(r"^([a-z]+) \(([0-9]+)\)( -> (.*?))?$").unwrap();
     for line in edges_string.lines() {
-        println!("Line is {}", line);
         let captured = edges.captures(line).expect("Could not parse line");
         let head = captured.get(1).unwrap().as_str();
         let weight = captured.get(2).unwrap().as_str();
@@ -30,6 +38,7 @@ fn main() {
             node = tree.new_node(head);
             nodes.insert(head, node);
         }
+        weights.insert(head, weight.parse().unwrap());
         let mut child_nodes: Vec<&str> = Vec::new();
         if children.is_some() {
             for child in children.unwrap().as_str().split(", ") {    
@@ -47,10 +56,48 @@ fn main() {
         //println!("Captured is {:?}", captured);
     }
 
-    //println!("Tree {:?}", tree);
+    println!("Tree {:?}", tree);
+    let mut head = None;
+
     for node in nodes {
         if node.1.ancestors(tree).into_iter().count() == 1 {
-            println!("Node {:?}", node);
+            head = Some(node.1);
+            println!("Head is {:?}", node.0);
+            break;
+        }
+    }
+    if head.is_some() {
+        let mut current = head.unwrap();
+        let mut previous_diff: i32 = 0;
+        loop {
+            let mut child_weights: Vec<i32> = Vec::new();
+            println!("Node {:?} has inherent weight {}", tree[current].data, weights.get(tree[current].data).unwrap());
+            for node in current.children(tree) {
+                let node_weight = weight(node, tree, &weights);
+                println!("Node {:?} has total weight {}", tree[node].data, node_weight);
+                child_weights.push(node_weight);
+            }
+            let max = child_weights.iter().max().unwrap();
+            let min = child_weights.iter().min().unwrap();
+            
+            let num_maxes = child_weights.iter().enumerate().filter(|x| x.1 == max).count();
+            let num_mins = child_weights.iter().enumerate().filter(|x| x.1 == min).count();
+ 
+            if num_maxes == 1 {
+                let mut maxes = child_weights.iter().enumerate().filter(|x| x.1 == max);
+                let child_index = maxes.next().unwrap().0;
+                previous_diff = *min - *max;
+                current = current.children(tree).into_iter().skip(child_index).next().unwrap();
+            } else if num_mins == 1 {
+                let mut mins = child_weights.iter().enumerate().filter(|x| x.1 == min);
+                let child_index = mins.next().unwrap().0;
+                previous_diff = *max - *min;
+                current = current.children(tree).into_iter().skip(child_index).next().unwrap();
+            } else {
+                println!("Previous was {}", previous_diff);
+                println!("Inherent weight should be {}", *weights.get(tree[current].data).unwrap() + previous_diff);
+                break;
+            }
         }
     }
 }
