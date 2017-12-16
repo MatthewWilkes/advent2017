b319607d4355498bfa9b395acd5c2f9af8f8a8f2
Matthew Wilkes <git@matthewwilkes.name>
2017-12-16 12:40:53 +0000

    Also count number of garbage characters ignored

--- nine/src/main.rs
+++ nine/src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
 
-fn check_group(group: &str) -> u32 {
+fn check_group(group: &str) -> (u32, u32) {
     let mut depth = 0;
     let mut cancelled = false;
     let mut garbage = false;
     let mut score = 0;
+    let mut num_cancelled = 0;
 
     for char in group.chars() {
         if cancelled {
@@ -13,7 +14,7 @@ fn check_group(group: &str) -> u32 {
             match char {
                 '!' => cancelled = !cancelled,
                 '>' => garbage = false,
-                _ => (),
+                _ => num_cancelled += 1,
             }
         } else {
             match char {
@@ -29,52 +30,87 @@ fn check_group(group: &str) -> u32 {
             }
         }
     }
-    return score;
+    return (score, num_cancelled);
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read input");
-    println!("{} -> {}", input, check_group(input.trim()));
+    println!("{} -> {:?}", input, check_group(input.trim()));
 }
 
 
 #[test]
 fn test_trivial_group() {
-    assert_eq!(check_group("{}"), 1);
+    assert_eq!(check_group("{}").0, 1);
 }
 
 #[test]
 fn test_deeply_nested() {
-    assert_eq!(check_group("{{{}}}"), 6);
+    assert_eq!(check_group("{{{}}}").0, 6);
 }
 
 #[test]
 fn test_multiple_nested() {
-    assert_eq!(check_group("{{},{}}"), 5);
+    assert_eq!(check_group("{{},{}}").0, 5);
 }
 
 #[test]
 fn test_differnet_nestings() {
-    assert_eq!(check_group("{{{},{},{{}}}}"), 16);
+    assert_eq!(check_group("{{{},{},{{}}}}").0, 16);
 }
 
 #[test]
 fn test_garbage_in_group() {
-    assert_eq!(check_group("{<a>,<a>,<a>,<a>}"), 1);
+    assert_eq!(check_group("{<a>,<a>,<a>,<a>}").0, 1);
 }
 
 #[test]
 fn test_garbage_nested() {
-    assert_eq!(check_group("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
+    assert_eq!(check_group("{{<ab>},{<ab>},{<ab>},{<ab>}}").0, 9);
 }
 
 #[test]
 fn test_double_cancellations() {
-    assert_eq!(check_group("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
+    assert_eq!(check_group("{{<!!>},{<!!>},{<!!>},{<!!>}}").0, 9);
 }
 
 #[test]
 fn test_cancelled_garbage_escaped() {
-    assert_eq!(check_group("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
+    assert_eq!(check_group("{{<a!>},{<a!>},{<a!>},{<ab>}}").0, 3);
+}
+
+#[test]
+fn test_no_garbage() {
+    assert_eq!(check_group("<>").1, 0);
+}
+
+#[test]
+fn test_some_garbage() {
+    assert_eq!(check_group("<random characters>").1, 17);
+}
+
+#[test]
+fn test_multiple_garbage_statts() {
+    assert_eq!(check_group("<<<<>").1, 3);
+}
+
+#[test]
+fn test_cancelled_ending() {
+    assert_eq!(check_group("<{!>}>").1, 2);
+}
+
+#[test]
+fn test_double_cancelled_inside_garbage() {
+    assert_eq!(check_group("<!!>").1, 0);
+}
+
+#[test]
+fn test_triple_cancelled_inside_garbage() {
+    assert_eq!(check_group("<!!!>>").1, 0);
+}
+
+#[test]
+fn test_complex_garbage() {
+    assert_eq!(check_group("<{o\"i!a,<{i<a>").1, 10);
 }
